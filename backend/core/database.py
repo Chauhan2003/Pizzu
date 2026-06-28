bc509cbb7fed256ff0050ed91a89ddf6fab3b325
Gagan Chauhan <chauhangagan.be03@gmail.com>
2026-06-29 00:59:55 +0530

    Implemented alembic and added user schema

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 from core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
 
 SessionLocal = sessionmaker(
     bind=engine,
---
+++ backend/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True)
+    password = Column(String, nullable=False)
+
+    phone = Column(String, unique=True)
+
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
